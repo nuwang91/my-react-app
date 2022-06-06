328235c8990ebfbe174b064c0fbdbeb9d40f6ef7
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-06-06 19:47:39 +0530

    feat(newexpense): created new expense component

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import "./NewExpense.scss"
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = () => {
+    return (
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    )
+}
+
+export default NewExpense;
